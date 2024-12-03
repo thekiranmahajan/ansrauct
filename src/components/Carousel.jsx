924b3f7e409435460be832315d66568d115fe94a
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-03 17:51:24 +0530

    fix: abstract the carousel sub components + constants and make it more dynamic and optimised

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { motion, useMotionValue } from "motion/react";
+import CarouselDots from "./CarouselDots";
+import CarouselEdges from "./CarouselEdges";
+import { carouselData } from "../utils/carouselData";
+import { AUTO_DELAY, DRAG_BUFFER, SPRING_OPTIONS } from "../utils/constants";
+import CarouselImages from "./CarouselImages";
 
-const ONE_SECOND = 1000;
-const AUTO_DELAY = ONE_SECOND * 10;
-const DRAG_BUFFER = 50;
-
-const SPRING_OPTIONS = {
-  type: "spring",
-  mass: 3,
-  stiffness: 400,
-  damping: 50,
-};
-
-const Carousel = ({ images }) => {
-  const [dragging, setDragging] = useState(false);
+const Carousel = () => {
   const [imgIndex, setImgIndex] = useState(0);
 
   const dragX = useMotionValue(0);
@@ -23,11 +16,11 @@ const Carousel = ({ images }) => {
       const x = dragX.get();
 
       if (x === 0) {
-        setImgIndex((pv) => {
-          if (pv === images?.length - 1) {
+        setImgIndex((prev) => {
+          if (prev === carouselData?.length - 1) {
             return 0;
           }
-          return pv + 1;
+          return prev + 1;
         });
       }
     }, AUTO_DELAY);
@@ -37,7 +30,8 @@ const Carousel = ({ images }) => {
 
   const onDragEnd = () => {
     const x = dragX.get();
-    if (x <= -DRAG_BUFFER && imgIndex < images.length - 1) {
+
+    if (x <= -DRAG_BUFFER && imgIndex < carouselData?.length - 1) {
       setImgIndex((prev) => prev + 1);
     } else if (x >= DRAG_BUFFER && imgIndex > 0) {
       setImgIndex((prev) => prev - 1);
@@ -45,67 +39,27 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <div>
+    <div className="relative overflow-hidden bg-neutral-900 py-8">
       <motion.div
         drag="x"
         dragConstraints={{
           left: 0,
           right: 0,
         }}
-        animate={{
-          translateX: `-${imgIndex * 100}%`,
-        }}
         style={{
           x: dragX,
         }}
+        animate={{
+          translateX: `-${imgIndex * 100}%`,
+        }}
         transition={SPRING_OPTIONS}
         onDragEnd={onDragEnd}
-        className="carousel flex cursor-grab items-center active:cursor-grabbing"
+        className="flex cursor-grab items-center active:cursor-grabbing"
       >
-        {images?.map((imageSrc, index) => {
-          return (
-            <motion.div
-              key={index}
-              className="relative aspect-video w-screen shrink-0 overflow-hidden bg-neutral-800"
-            >
-              <div
-                style={{
-                  backgroundImage: `url(${imageSrc.image})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}
-                className="absolute inset-0"
-              ></div>
-
-              <div className="absolute inset-0 bg-black/50"></div>
-
-              <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-                <h2 className="text-3xl font-bold">{imageSrc.title}</h2>
-                <p className="text-[#fffdfde8] mt-4 w-3/4 text-center">{imageSrc.description}</p>
-              </div>
-            </motion.div>
-          );
-        })}
+        <CarouselImages imgIndex={imgIndex} />
       </motion.div>
-      <Dots images={images} imgIndex={imgIndex} setImgIndex={setImgIndex} />
-    </div>
-  );
-};
-
-const Dots = ({ images, imgIndex, setImgIndex }) => {
-  return (
-    <div className="absolute -mt-8 flex w-full justify-center gap-2">
-      {images?.map((_, idx) => {
-        return (
-          <button
-            key={idx}
-            onClick={() => setImgIndex(idx)}
-            className={`h-3 w-3 rounded-full transition-colors ${
-              idx === imgIndex ? "bg-neutral-50" : "bg-neutral-500"
-            }`}
-          />
-        );
-      })}
+      <CarouselDots imgIndex={imgIndex} setImgIndex={setImgIndex} />
+      <CarouselEdges />
     </div>
   );
 };
