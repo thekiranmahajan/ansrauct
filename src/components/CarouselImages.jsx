cb8bf387adb4771070f208e740d8ef0e784b757b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-03 19:32:08 +0530

    fix: responsive text for carousel

--- src/components/CarouselImages.jsx
+++ src/components/CarouselImages.jsx
@@ -22,10 +22,10 @@ const CarouselImages = ({ imgIndex }) => {
             className="relative flex aspect-video w-screen shrink-0 items-start justify-center overflow-hidden rounded-xl bg-neutral-800 object-cover"
           >
             <div className="flex h-full w-full flex-col items-center justify-center bg-black/50 px-4 py-2 pt-2 text-white sm:p-6 md:p-10">
-              <h2 className="text-xl font-bold sm:text-2xl md:text-3xl">
+              <h2 className="text-xl font-bold sm:text-2xl md:text-4xl lg:text-6xl">
                 {imageData?.title}
               </h2>
-              <p className="mt-1 w-full text-center text-xs text-gray-50 sm:text-base md:mt-4 md:w-3/4 lg:w-2/3">
+              <p className="mt-1 w-full text-center text-xs text-gray-50 sm:text-base md:mt-4 md:w-3/4 md:text-lg lg:w-2/3 lg:text-xl">
                 {imageData?.description}
               </p>
             </div>
