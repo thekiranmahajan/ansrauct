a4d2bee0dd21246a97fc27871b16e21ee04f26cb
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-04 18:30:27 +0530

    feat: add profile images to testimonialData object

---
+++ src/assets/testimonials/index.js
@@ -0,0 +1,2 @@
+export { default as profile1 } from "./profile1.png";
+export { default as profile2 } from "./profile2.png";
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,33 +1,32 @@
+import { profile1, profile2 } from "../assets/testimonials";
 const color_ModerateViolet = "hsl(263, 55%, 52%)";
 const color_VeryDarkGrayishBlue = "hsl(217, 19%, 35%)";
 
 export const testimonialData = [
   {
     id: 1,
-    className: "col-span-2 row-span-1",
     bgColor: color_ModerateViolet,
-    profileImg: "",
+    profileImg: profile1,
     name: "Natasha Russell",
     position: "PhD, Global Cocoa Supplier - Quality Assurance Lead",
     companyName: "Mars",
     feedback:
       "I just wanted to congratulate you on the Treenuts material school, it was truly excellent,it made me want to run out and buy some nuts for their nutritional benefits. The course was extremely clear, with a very good flow, and I learnt a lot. I also really liked the detail that you went into in the cleaning etc…I think this is quite similar to cocoa, so I would definitely recommend my cocoa SQA's to take this course.",
-    socials: "",
+    Linkedin: "",
   },
   {
     id: 2,
     bgColor: color_VeryDarkGrayishBlue,
-    profileImg: "",
+    profileImg: profile2,
     name: "David Gellie",
     position: "Global Pet Nutrition Supplier Quality Assurance",
     companyName: "Mars Pet Nutrition",
     feedback:
       "You should feel immensely proud of his team they have delivered in spades such a step change in learning, in quality, service and ownership. This Material school you and the team should feel immensely proud of.",
-    socials: "",
+    Linkedin: "",
   },
   {
     id: 3,
-    className: "md:row-span-2",
     bgColor: color_ModerateViolet,
     profileImg: "",
     name: "Nicole Röhrs",
@@ -35,29 +34,28 @@ export const testimonialData = [
     companyName: "Mars GMBH",
     feedback:
       "I'm really impressed by the speed you conducted the latest updates and refinements. Great job and many thanks for your energy, flexibility and persistence!",
-    socials: "",
+    Linkedin: "",
   },
   {
     id: 4,
     bgColor: color_ModerateViolet,
-    profileImg: "",
+    profileImg: profile2,
     name: "Dana Hafeed",
     position: "VP P&O EM",
     companyName: "Mars Wrigley",
     feedback:
       "I have completed this course, I must say that it is certainly among the best online training that I had so far. Thanks a lot to all the people that have contributed to putting this together, and Thanks for giving me the opportunity to participate.",
-    socials: "",
+    Linkedin: "",
   },
   {
     id: 5,
-    className: "md:col-span-2",
     bgColor: color_VeryDarkGrayishBlue,
-    profileImg: "",
+    profileImg: profile1,
     name: "Victoria Veninga",
     position: "L&D Talent & Change Projects",
     companyName: "Mars",
     feedback:
       "I would like to share how much I am enjoying working with you all. I am constantly impressed with your team’s ability to grasp the key messages from our existing materials, and to translate this into engaging and effective scripts and storyboards. It seems effortless, but I know it certainly is not.  Your team is also incredibly skilled at understanding my feedback and adapting these to incorporate the improvements we co-create together. The efforts of Vanessa and everyone in the wider design (and now development) team are sincerely appreciated, as we work towards our goals.",
-    socials: "",
+    Linkedin: "",
   },
 ];
