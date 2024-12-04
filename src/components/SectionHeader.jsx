ef51f4df5b4724877abead538427537b9388e3fd
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-04 20:01:20 +0530

    feat: add animation to the sectionHeader dot

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { motion, useInView } from "motion/react";
+import React, { useRef } from "react";
 
 const SectionHeader = ({ heading, subHeading }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   return (
-    <div className="relative flex min-h-40 w-full flex-col items-center justify-center text-center">
+    <div
+      ref={ref}
+      className="relative flex min-h-40 w-full flex-col items-center justify-center text-center"
+    >
       <h2 className="z-10 bg-white/50 pb-0.5 text-2xl font-bold uppercase leading-snug text-primary sm:text-3xl md:text-3xl lg:text-4xl">
         {heading}
       </h2>
-      <span className="h-2 w-2 rounded-full bg-primary"></span>
+      <motion.span
+        className="z-10 h-2 w-2 rounded-full bg-primary opacity-70"
+        animate={isInView ? { y: [0, -8, 0] } : {}}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      ></motion.span>
       <h4 className="z-10 bg-white/50 pt-0.5 text-base text-secondary md:text-lg lg:text-xl">
         {subHeading}
       </h4>
