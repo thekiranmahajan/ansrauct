20fcfb8336bb5296060beb0d76e30aa3cb843d81
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-04 18:26:09 +0530

    style: fix font sizes and make it responsive

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const SectionHeader = ({ heading, subHeading }) => {
   return (
     <div className="relative flex min-h-40 w-full flex-col items-center justify-center text-center">
-      <h2 className="z-10 bg-white/50 p-2 text-2xl font-semibold text-primary sm:text-3xl">
+      <h2 className="z-10 bg-white/50 p-2 text-3xl font-bold leading-snug text-primary sm:text-3xl md:text-4xl lg:text-5xl">
         {heading}
       </h2>
-      <h4 className="z-10 bg-white/50 p-2 text-secondary">{subHeading}</h4>
+      <h4 className="z-10 bg-white/50 p-2 font-bold text-secondary text-sm md:text-lg lg:text-xl">
+        {subHeading}
+      </h4>
 
       <div className="pattern-dots pattern-secondary pattern-bg-white pattern-size-4 pattern-opacity-40 absolute z-0 h-full w-full"></div>
     </div>
