b80b57fc60f0a59bc95f7c8d887e08acd4ba2121
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-05 13:54:45 +0530

    feat: add resource link in icon and view button

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaFilePdf } from "react-icons/fa6";
-const ResourceCard = ({ title, description }) => {
+const ResourceCard = ({ title, description, ResourceLink }) => {
   return (
     <div className="m-5">
       <div className="mx-2 mt-10 grid max-w-screen-lg grid-cols-1 space-x-8 overflow-hidden rounded-lg border text-gray-700 shadow transition hover:shadow-lg sm:mx-auto sm:grid-cols-5">
-        <a href="#" className="col-span-1 m-auto">
+        <a href={ResourceLink} className="col-span-1 m-auto">
           <div className="relative h-full w-full overflow-hidden p-2">
             <FaFilePdf className="text-8xl text-primary transition duration-300 hover:scale-110" />
           </div>
@@ -17,7 +17,7 @@ const ResourceCard = ({ title, description }) => {
 
           <div className="flex flex-col text-gray-700 sm:flex-row">
             <a
-              href="#"
+              href={ResourceLink}
               className="my-5 rounded-md bg-orange-600 px-8 py-2 text-center text-white transition hover:scale-105"
             >
               View
