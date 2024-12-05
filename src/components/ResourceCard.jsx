f0ad5e0562b9f78c0907ba7f8960feeacf81beb9
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-05 14:12:21 +0530

    feat: utilize button component for view button

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaFilePdf } from "react-icons/fa6";
-const ResourceCard = ({ title, description, ResourceLink }) => {
+import Button from "./Button";
+const ResourceCard = ({ title, description, link, btnText }) => {
   return (
     <div className="m-5">
       <div className="mx-2 mt-10 grid max-w-screen-lg grid-cols-1 space-x-8 overflow-hidden rounded-lg border text-gray-700 shadow transition hover:shadow-lg sm:mx-auto sm:grid-cols-5">
-        <a href={ResourceLink} className="col-span-1 m-auto">
+        <a href={link} className="col-span-1 m-auto">
           <div className="relative h-full w-full overflow-hidden p-2">
             <FaFilePdf className="text-8xl text-primary transition duration-300 hover:scale-110" />
           </div>
@@ -15,13 +16,8 @@ const ResourceCard = ({ title, description, ResourceLink }) => {
           </a>
           <p className="overflow-hidden text-sm">{description}</p>
 
-          <div className="flex flex-col text-gray-700 sm:flex-row">
-            <a
-              href={ResourceLink}
-              className="my-5 rounded-md bg-orange-600 px-8 py-2 text-center text-white transition hover:scale-105"
-            >
-              View
-            </a>
+          <div className="mt-5 flex w-full items-center xl:mt-10">
+            <Button link={link} btnText={btnText} />
           </div>
         </div>
       </div>
