cd7288899a610f9ae14b31828f7697b275dd7fcf
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-09 11:40:19 +0530

    fix: infinte logo carousel

--- src/components/LogoCarousel.jsx
+++ src/components/LogoCarousel.jsx
@@ -4,28 +4,30 @@ import { motion } from "motion/react";
 
 const LogoCarousel = () => {
   return (
-    <div className="flex h-24 w-11/12 overflow-hidden">
-      <div className="flex">
-        <motion.div
-          initial={{ x: 0 }}
-          animate={{ x: "-100%" }}
-          transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
-          className="flex flex-shrink-0"
-        >
-          {firstHalfIcons.map((logo, index) => (
-            <img className="w-20 pr-10" src={logo} key={index} />
-          ))}
-        </motion.div>
-        <motion.div
-          initial={{ x: 0 }}
-          animate={{ x: "-100%" }}
-          transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
-          className="flex flex-shrink-0"
-        >
-          {firstHalfIcons.map((logo, index) => (
-            <img className="w-20 pr-10" src={logo} key={index} />
-          ))}
-        </motion.div>
+    <div className="container flex h-40 w-full items-center justify-center overflow-hidden">
+      <div className="container mx-auto">
+        <div className="flex">
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+            className="flex flex-shrink-0"
+          >
+            {firstHalfIcons.map((logo, index) => (
+              <img className="w-32 pr-20" src={logo} key={index} />
+            ))}
+          </motion.div>
+          <motion.div
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+            className="flex flex-shrink-0"
+          >
+            {firstHalfIcons.map((logo, index) => (
+              <img className="w-32 pr-20" src={logo} key={index} />
+            ))}
+          </motion.div>
+        </div>
       </div>
     </div>
   );
